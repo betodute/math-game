dbd87949b7a9a8977a59688b39b4164172e279ae
Alberto Duarte <betodute@gmail.com>
2022-06-22 17:15:05 -0700

    Everything but timer working
    Almost full functionality, look up how to properly add time to setInterval

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
 
   $("#userForm").on('submit', function(event) {
     event.preventDefault();
-    startGame();
     checkAnswer();
   });
+
+  $('.start-game').on('click', function() {
+    $('.game-over').css("display", "none");
+    $('.try-again').css("display", "none");
+    startGame();
+  })
 
   problemLvlOne();
 })
@@ -14,13 +19,15 @@ $(document).ready(function() {
 var answer = 0;
 var timeRemaining = 10;
 var gameActive = 0;
+var score = 0;
 
 var checkAnswer = () => {
   var userAnswer = parseInt($('#userInput').val());
   $('#userInput').val('');
   if (answer === userAnswer ) {
     $('.try-again').css("display", "none");
-    timeRemaining += 5;
+    timeRemaining += 2;
+    addScore();
     problemLvlOne();
   } else {
     tryAgain();
@@ -28,19 +35,22 @@ var checkAnswer = () => {
 };
 
 var startGame = () => {
-  if (gameActive === 0) {
-    timeRemaining = 10;
-    gameActive = 1;
-    var startTimer = setInterval(function(){
-      timeRemaining -= 1;
-      $('.timer').html(timeRemaining);
-      if (timeRemaining <= 0) {
-        gameOver();
-        clearInterval(startTimer);
-      }
-    }, 1000)
-  };
+  timeRemaining = 10;
+  gameActive = 1;
+  // I know the following function name is too long but it's worth 3000 cute points!
+  timerControlStation()
 } 
+
+var timerControlStation = () => {
+    var startTimer = setInterval(function(){
+    timeRemaining -= 1;
+    $('.timer').html(timeRemaining);
+    if (timeRemaining <= 0) {
+      gameOver();
+      clearInterval(startTimer);
+    }
+    }, 1000)
+};
 
 var createProblem = (maxNum) => {
   var firstNum = randomNum(maxNum);
@@ -49,12 +59,19 @@ var createProblem = (maxNum) => {
   return `${firstNum} + ${secondNum} =`
 };
 
-var gameOver = () => {
-  gameActive = 0;
-};
+var addScore = () => {
+  score += 1;
+  $('.score').html(score);
+}
+
 
 var tryAgain = function() {
   $('.try-again').css("display", "block");
+}
+
+var gameOver = function() {
+  gameActive = 0;
+  $('.game-over').css("display", "block");
 }
 
 var problemLvlOne = function () {
